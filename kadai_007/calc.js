49cd94814d7c2a7642b8b81cd9d8122e67ee2339
nmnm8639 <143925460+nmnm8639@users.noreply.github.com>
2023-09-19 18:17:26 +0900

    kadai_007を修正

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,11 +1,11 @@
-let num = Math.floor(Math.random() * 15);
+let num = Math.floor(Math.random() * 100);
 
-if (num === 3 || num === 6 || num === 9 || num === 12){
-  console.log('3の倍数です')
-}if(num === 5 || num === 10 || num ===15) {
-  console.log('5の倍数です')
-}if(num === 15) {
+if(num % 3 === 0 && num % 5 === 0) {
   console.log('3と5の倍数です')
-}else if(num === 1 || num === 2 || num === 4 || num === 7 || num === 8 || num === 11 || num === 13 || num === 14) {
+}else if (num % 3 === 0) {
+  console.log('3の倍数です')
+}else if(num % 5 === 0) {
+  console.log('5の倍数です')
+}else {
   console.log(num)
 }
