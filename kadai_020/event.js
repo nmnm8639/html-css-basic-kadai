0a8dfdf9b2abd710e97ff691a678eb27583c0059
nmnm8639 <143925460+nmnm8639@users.noreply.github.com>
2023-10-07 16:40:06 +0900

    kadai

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,7 +1,5 @@
-const Btn = document.getElementById(btn);
 
-const Text = document.getElementById(text);
-
-btn.addEventListener('click', () => {
-text.textContent = 'ボタンがクリックされました';
+btn.addEventListener('click',()=> {
+   text.textContent = 'ボタンがクリックされました'
 });
+
